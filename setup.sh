a11c9f7e5d3cd3b9b5e523fb1fdd33c3527347a8
yjn0 <49813832+yjn0@users.noreply.github.com>
2023-09-12 18:50:37 +0000

    add oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,17 @@
-# Git aliases
+BASE_DIR="$HOME/dotfiles_config"
+mkdir $BASE_DIR
+echo "Generated from dotfiles repository (https://github.com/yjn0/dotfiles)" >> $BASE_DIR/README
+
+# git aliases
 git config alias.st status
 git config alias.br branch
 git config alias.co checkout
 git config alias.ci commit
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -y"
+cp ./.zshrc $HOME/.zshrc
+
+# comfyline_prompt (oh-my-zsh theme)
+git clone "https://gitlab.com/imnotpua/comfyline_prompt.git"
+echo "source comfyline_prompt/comfyline.zsh-theme" >> $HOME/.zshrc
