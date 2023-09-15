4c24bb6455e8036c120bd8663d2d0093cc37b52a
yjn0 <49813832+yjn0@users.noreply.github.com>
2023-09-15 14:21:00 -0400

    add Meslo NF

--- setup.sh
+++ setup.sh
@@ -4,6 +4,13 @@ git config alias.br branch
 git config alias.co checkout
 git config alias.ci commit
 
+# install fonts
+mkdir Meslo
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
+sudo unzip Meslo.zip -d /usr/share/fonts
+sudo fc-cache -f -v
+rm Meslo.zip
+
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -Y"
 
