54d4fffc131d7f47f2ef5fe60d8614af675ae043
mirilabs <49813832+mirilabs@users.noreply.github.com>
2024-01-23 15:34:52 -0500

    remove oh-my-posh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-# upgrade yarn to v3
+# upgrade yarn
 yarn set version stable
 
 # git aliases
@@ -7,18 +7,8 @@ git config alias.br branch
 git config alias.co checkout
 git config alias.ci commit
 
-# install fonts
-mkdir Meslo
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Meslo.zip
-sudo unzip Meslo.zip -d /usr/share/fonts
-sudo fc-cache -f -v
-rm Meslo.zip
-
 # install oh-my-zsh
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -Y"
-
-# install oh-my-posh
-curl -s https://ohmyposh.dev/install.sh | sudo bash -s
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) -Y"
 
 # copy config files
 cp ./.zshrc $HOME/.zshrc
